668469671ed5a79806774187e4091d67ed75b1af
Firthuns2021 <86574425+Firthuns2021@users.noreply.github.com>
2022-02-10 19:54:44 +0100

    sesion clase dia  10/02/2022, faltaria correguir erroes

--- src/EventListener/OnLoginListener.php
+++ src/EventListener/OnLoginListener.php
@@ -2,11 +2,17 @@
 
 namespace App\EventListener;
 
+use App\Repository\ClienteRepository;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class OnLoginListener
 {
+
+    private $repo;
+    public function __construct(ClienteRepository  $repo){
+        $this->repo = $repo;
+    }
 
     public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event){
         $data = $event->getData();
@@ -14,7 +20,19 @@ class OnLoginListener
         if(!$user instanceof UserInterface){
             return;
         }
-        $data['user_id'] = $user->getid();
+
+        foreach ($user->getRoles() as $rol){
+            if ($rol == 'ROLE_CLIENTE'){
+                $cliente = $this->repo->findOneBy(['user'=>$user->getId()]);
+            }
+        }
+        if($cliente){
+            $id_cliente = $cliente->getId();
+            $data['cliente']=$id_cliente;
+        }
+
+//        $data['user_id'] = $user->getid();
+        $data['id'] = $user->getid();
         $event->setData($data);
     }
 
